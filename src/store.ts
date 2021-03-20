42088f4512eb3927a665404645c5a78691430062
lucien dulac <lucien.dulac@gmail.com>
2021-03-20 19:38:38 +0100

    add folder for store types

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
-import type { Auth } from './types/auth';
+import type { Auth } from './types';
 
 export const auth = writable<Auth>({
   isAuthenticated: false,
